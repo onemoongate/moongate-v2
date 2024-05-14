833e36a775bac8a688300a5b449032342b7b5586
ZeroTimeDrift <403.hevar@gmail.com>
2024-05-14 03:59:51 +0100

    feat: v2 moongate

---
+++ my-project/src/app/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Image from "next/image";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import App from "./app";
+import { ConnectionProvider, WalletProvider, useWallet } from '@solana/wallet-adapter-react';
+
+import { MoongateWalletAdapter } from "@moongate/moongate-adapter"
+import { useMemo } from "react";
+
+export default function Home() {
+  const wallets = useMemo(
+    () => [
+      /**
+       * Wallets that implement either of these standards will be available automatically.
+       *
+       *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
+       *     (https://github.com/solana-mobile/mobile-wallet-adapter)
+       *   - Solana Wallet Standard
+       *     (https://github.com/anza-xyz/wallet-standard)
+       *
+       * If you wish to support a wallet that supports neither of those standards,
+       * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+       * in the npm package `@solana/wallet-adapter-wallets`.
+       */
+      new MoongateWalletAdapter(),
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+  return (
+    <WalletProvider wallets={wallets} autoConnect>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <App />
+      </main>
+    </WalletProvider>
+  );
+}
